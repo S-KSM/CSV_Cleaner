51f925e498bab7627f94967374921b7f20e71368
Shobeir <shobeir.mazinani@gmail.com>
2016-06-22 16:31:58 -0700

    adding a groupby
    grouped report

--- CSV_Cleaner.py
+++ CSV_Cleaner.py
@@ -1,30 +1,30 @@
 
 # coding: utf-8
 
-# In[33]:
+# In[1]:
 
 import pandas as pd
 
 
-# In[34]:
+# In[2]:
 
 db = pd.read_csv("results.csv")
 
 
-# In[35]:
+# In[3]:
 
 dummies = pd.get_dummies(db,columns=["status"])
 dummies_smaller = dummies[["position","status_Email Opened","status_Email Sent","status_Success"]]
 dummies_smaller.columns = ["position","Email_Opened","Email_Sent","Success"]
 
 
+# In[8]:
+
+dummies_smaller.groupby(("position")).sum().reset_index().to_csv("smaller_output.csv",",",index= None)
+
+
 # In[36]:
 
-dummies.to_csv("Output.csv",",",index= None)
-dummies_smaller.to_csv("smaller_output.csv",",",index= None)
-
-
-# In[ ]:
-
-
+#dummies.to_csv("Output.csv",",",index= None)
+#dummies_smaller.to_csv("smaller_output.csv",",",index= None)
 
